f72a5257bfae00fd6d9f5a1e63f7e683ca83dd99
elliot <jiang0426@sjtu.edu.cn>
2020-07-08 10:34:20 +0800

    ver-2
    slightly changed some css

--- gather_ticket/src/Components/Nav.js
+++ gather_ticket/src/Components/Nav.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import navstyle from './Nav.module.css'
 import { Link } from 'react-router-dom';
+import search from '../Assets/images/ico/search2.png'
 
 export default class nav extends Component {
 
@@ -14,15 +15,15 @@ export default class nav extends Component {
             <div className={navstyle.nav}>
                 <div className={navstyle.navson}>
                     <div className={navstyle.logo}>
-                        <img src='http://localhost:8081/images/logo.png' ></img>
+                        <img src={require('../Assets/images/logo.png')} ></img>
                     </div>
                     <div className={navstyle.global}>
                         <div className={navstyle.globalt}>
                             <div className={navstyle.position}>
-                                <img src='http://localhost:8081/images/ico/location.png' ></img>
+                                <img src={require('../Assets/images/ico/location.png')} ></img>
                             </div>
                             <div className={navstyle.text}>全国</div>
-                            <img src='http://localhost:8081/images/ico/down.png' className={navstyle.downarrow}></img>
+                            <img src={require('../Assets/images/ico/down.png')} className={navstyle.downarrow}></img>
                         </div>
                     </div>
                     <div className={navstyle.navbar}>
@@ -32,19 +33,19 @@ export default class nav extends Component {
                     <div className={navstyle.search}>
                         <input type="text" className={navstyle.input} placeholder="搜索明星、演出、体育赛事">
                         </input>
-                        <img src='http://localhost:8081/images/ico/search.png' className={navstyle.searchimg}></img>
-                        <div className={navstyle.inputnext}>搜索</div>
+
+                        <div className={navstyle.inputtext}><img src={search} className={navstyle.searchimg}></img></div>
                     </div>
                     <div className={navstyle.loginbox}>
-                        <img src='http://localhost:8081/images/ico/login.png' className={navstyle.loginimg}></img>
+                        <img src={require('../Assets/images/ico/login.png')} className={navstyle.loginimg}></img>
                         <div className={navstyle.logintext}><Link to="/login">登录</Link></div>
                     </div>
                     <div className={navstyle.downbox}>
-                        <img src='http://localhost:8081/images/ico/download.png' className={navstyle.loginimg}></img>
+                        <img src={require('../Assets/images/ico/download.png')} className={navstyle.loginimg}></img>
                         <div className={navstyle.logintext}>下载</div>
                     </div>
                     <div className={navstyle.downbox}>
-                        <img src='http://localhost:8081/images/ico/english.png' className={navstyle.loginimg}></img>
+                        <img src={require('../Assets/images/ico/english.png')} className={navstyle.loginimg}></img>
                         <div className={navstyle.logintext}>English</div>
                     </div>
                 </div>
