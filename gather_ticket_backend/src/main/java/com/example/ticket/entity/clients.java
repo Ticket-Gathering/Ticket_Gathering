df2ab299258cc106bccf326b51024ea92da6f8ba
hurt1 <55347567+hurt1@users.noreply.github.com>
2020-07-13 16:32:02 +0800

    后端 添加了4个entity

---
+++ gather_ticket_backend/src/main/java/com/example/ticket/entity/clients.java
@@ -0,0 +1,31 @@
+package com.example.ticket.entity;
+
+import com.fasterxml.jackson.annotation.JsonIdentityInfo;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.ObjectIdGenerators;
+import lombok.Data;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import java.util.Date;
+
+@Data
+@Entity
+@Table(name = "clients")
+@JsonIgnoreProperties(value = {"handler","hibernateLazyInitializer","fieldHandler"})
+@JsonIdentityInfo(generator = ObjectIdGenerators.PropertyGenerator.class,property = "clientID")
+public class clients {
+    @Id
+    @Column(name = "user_id")
+    private int userId;
+
+    private String name;
+    private String nickname;
+    private int id;
+    private String tel;
+    private String email;
+    private Date birth;
+    private int gender;
+}
---
+++ gather_ticket_backend/src/main/java/com/example/ticket/entity/show.java
@@ -0,0 +1,37 @@
+package com.example.ticket.entity;
+
+import com.fasterxml.jackson.annotation.JsonIdentityInfo;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.ObjectIdGenerators;
+import lombok.Data;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Data
+@Entity
+@Table(name = "show")
+@JsonIgnoreProperties(value = {"handler","hibernateLazyInitializer","fieldHandler"})
+@JsonIdentityInfo(generator = ObjectIdGenerators.PropertyGenerator.class,property = "showID")
+public class show {
+    @Id
+    @Column(name = "id")
+    private int showId;
+
+    private String name;
+    private int category;
+    private int sub_category;
+    private String artists;
+    private String show_time;
+    private int venue_id;
+    private int is_eticket;
+    private int is_general_agent;
+    private int is_xuanzuo;
+    private Double price_low;
+    private Double price_high;
+    private int show_status;
+    private String img_url;
+    private String platform;
+}
