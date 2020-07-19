faa322f81d09f99f091708757a579f85984c289d
SJTU_ZYC <2824058153@qq.com>
2020-07-19 19:37:30 +0800

    订单确认界面初步框架

---
+++ gather_ticket/src/Views/Order/OrderPay.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+export default class OrderPay extends Component{
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return (
+            <div>
+                我是订单支付
+            </div>
+        );
+    }
+}
