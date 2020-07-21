200c1985ce22aadc18c44fad0083ba6bebb9e211
hurt1 <55347567+hurt1@users.noreply.github.com>
2020-07-21 14:50:52 +0800

    indent更新

--- gather_ticket_backend/src/main/java/com/example/ticket/controller/IndentController.java
+++ gather_ticket_backend/src/main/java/com/example/ticket/controller/IndentController.java
@@ -1,18 +1,18 @@
 package com.example.ticket.controller;
 
-import com.example.ticket.entity.ClientAuth;
-import com.example.ticket.entity.Indent;
 import com.example.ticket.service.IndentService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 public class IndentController {
     @Autowired
     private IndentService indentService;
 
     @RequestMapping("/addIndent")
-    public int checkUser(@RequestParam("username") String username, @RequestParam("showid") String showid, ){
-        return indentService.addIndent(username, showid,);
+    public int addIndent(@RequestParam("username") String username, @RequestParam("show_id") String show_id,@RequestParam("facevalue") Double facevalue,@RequestParam("num") Integer num,@RequestParam("payamount") Double payamount,@RequestParam("receiver_name") String receiver_name, @RequestParam("receiver_tel") String receiver_tel,@RequestParam("receiver_address") String receiver_address){
+        return indentService.addIndent(username, show_id,facevalue,num,payamount,receiver_name,receiver_tel,receiver_address);
     }
 }
