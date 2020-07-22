439c001ebf953ff791c7e74d3c01b61815d1e7e6
hurt1 <55347567+hurt1@users.noreply.github.com>
2020-07-22 08:39:44 +0800

    indent test

---
+++ gather_ticket_backend/src/test/java/com/example/ticket/Service/IndentServiceTest.java
@@ -0,0 +1,15 @@
+package com.example.ticket.Service;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import com.example.ticket.TicketApplication;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest(classes = TicketApplication.class)
+public class IndentServiceTest {
+    @Test
+    public void contextLoads(){
+    }
+}
