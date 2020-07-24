1f79fc0c22de5d49d4dcb091deb837099ea6594c
hurt1 <55347567+hurt1@users.noreply.github.com>
2020-07-24 14:59:33 +0800

    cypress admin

---
+++ gather_ticket/cypress/integration/admin.js
@@ -0,0 +1,23 @@
+describe('Classify', () => {
+    beforeEach(() =>{
+        cy.visit('http://localhost:3000/login')
+    })
+    it('classify',function () {
+        cy.get('[type="text"]').type("sjw");
+        cy.get('[type="password"]').type("123");
+        cy.get('.Login_loginBtn__ZRqpN').click();
+        cy.url().should('include', '/');
+        cy.get('.Nav_logintext__YP7s2 > a').click();
+        cy.url().should('include', '/self');
+        cy.get(':nth-child(3) > .ant-menu-submenu-title').click();
+        // cy.get('#sub3\$Menu > :nth-child(1)').click();
+        cy.get('[role="menuitem"][style="padding-left: 24px;"]').click();
+        cy.url().should('include', '/');
+        cy.get('.Nav_logintext__YP7s2 > a').click();
+        cy.url().should('include', '/login');
+        cy.get('[type="text"]').type("test");
+        cy.get('[type="password"]').type("test");
+        cy.get('.Login_loginBtn__ZRqpN').click();
+        cy.url().should('include', '/');
+    })
+})
